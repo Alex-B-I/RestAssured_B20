6134bbf2c4b6cd9bc86c2d428355960a7e47005f
Alex-B-I <alex.berloh@gmail.com>
2020-12-09 20:26:14 -0600

    day2 (given,when,then)

--- src/test/java/day02/SpartanTest.java
+++ src/test/java/day02/SpartanTest.java
@@ -3,6 +3,7 @@ package day02;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,8 +16,14 @@ public class SpartanTest {
 
     @BeforeAll
     public static void setUp(){
-        RestAssured.baseURI = "http://34.207.224.251:8000";
-        RestAssured.basePath = "/api";
+        // baseURI and basePath is static fields of RestAssured Class
+        // Since we static imported RestAssured, we can access all static field directly just like it's in our own class here
+        // you can use static way as below
+        //RestAssured.baseURI = "http://34.207.224.251:8000";
+        // or you can directly use as below
+        baseURI = "http://34.207.224.251:8000";
+        //RestAssured.basePath = "/api" ;
+        basePath = "/api";
         //baseURI+ basePath + whatever you provided in http method like GET post
         //for example:
         //get("/spartans") -->> get(baseURI + basePath + "/spartans")
@@ -25,7 +32,12 @@ public class SpartanTest {
     }
 
 
-
+    @AfterAll
+    public static void tearDown(){
+        // resetting the value of baseURI , basePath to original value
+   //     RestAssured.reset();
+        reset();
+    }
 
     @DisplayName("Testing /api/spartans endpoint")
     @Test
