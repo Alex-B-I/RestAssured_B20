fa779deb28d494d77c585a011106dd98e8b6dfe3
Alex-B-I <alex.berloh@gmail.com>
2020-12-15 20:22:32 -0600

    day6 POJO

---
+++ src/test/java/day06/JsonToJavaObject.java
@@ -0,0 +1,67 @@
+package day06;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utility.ConfigurationReader;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+
+public class JsonToJavaObject {
+
+    @BeforeAll
+    public static void setUp(){
+        baseURI = ConfigurationReader.getProperty("spartan.base_url");
+        basePath = "/api" ;
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        reset();
+    }
+    @DisplayName("Get 1 Data with Save Response Json As Java Object")
+    @Test
+    public void getOneSpartanAndSaveResponseJsonAsMap(){
+        Response response = given()
+                                     .auth().basic("admin","admin")
+                                     .log().all()
+                                     .pathParam("id",15).
+                           when()
+                                     .get("/spartans/{id}").prettyPeek() ;
+        //get jsonPath object
+        JsonPath jp = response.jsonPath();
+        Map<String,Object> responseMap =  jp.getMap("") ;
+        System.out.println("responseMap = " + responseMap);
+
+        /**
+         * {
+         *     "id": 5,
+         *     "name": "Sayeem",
+         *     "gender": "Male",
+         *     "phone": 1231231230
+         * }
+         * jsonPath to get whole json object is just empty string
+         *
+         * assume this is a car object
+         * {
+         *     "make":"Honda"
+         *     "color" : "white"
+         *     "engine" : {
+         *                   "type" : "v8"
+         *                   "horsepower" : 350
+         *                }
+         * }
+         * jsonPath for horse power -->> engine.horsepower
+         * jsonPath for engine itself -->> engine
+         * jsonPath for entire car JsonObject -->> ""
+         *
+         *
+         */
+    }
+
+}
