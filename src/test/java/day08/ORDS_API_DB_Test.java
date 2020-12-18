dc79e78935da07745007282455e1a2f29884c74f
Alex-B-I <alex.berloh@gmail.com>
2020-12-17 20:57:30 -0600

    day8

--- src/test/java/day08/ORDS_API_DB_Test.java
+++ src/test/java/day08/ORDS_API_DB_Test.java
@@ -3,6 +3,7 @@ package day08;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import pojo.BookCategory;
 import pojo.Country;
@@ -45,7 +46,7 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBAse {
     @DisplayName("Testing GET /regions/{region_id} Data Match Database Data")
     @Test
     public void testRegionDataFromResponseMatchDB_Data(){
-        int myID = 2 ;
+        int myID = 3 ;
         Response response = given()
                                  .pathParam("region_id", myID).
                         when()
@@ -55,6 +56,7 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBAse {
                                  .statusCode(200)
                                  .extract()
                                  .response();
+        // or you can do  as(Region.class) directly here
         Region r3 = response.as(Region.class);
         System.out.println("r3 = " + r3);
 
@@ -65,6 +67,69 @@ public class ORDS_API_DB_Test extends HR_ORDS_TestBAse {
         // verify the actual result from api response match expected database result
         assertThat(r3.getRegion_id()+"",equalTo(expectedResultMap.get("REGION_ID")));
         assertThat(r3.getRegion_name(),is(expectedResultMap.get("REGION_NAME")));
+
+    }
+    @DisplayName("Testing GET /regions/{region_id} Data Match Database Data With Both Maps")
+    @Test
+    public void testRegionDataFromResponseMatchDB_Data2() {
+        int myID = 3;
+        JsonPath jp = given()
+                .pathParam("region_id", myID).
+                        when()
+                .get("/regions/{region_id}").
+                        then()
+                .log().body()
+                .statusCode(200)
+                .extract()
+                .jsonPath();
+        // save the response json as a Map object
+        // Here we are calling the overloaded version of getMap method with 3 params
+        // 1. jsonPath String
+        // 2. Data type Map key
+        // 3. Data type Map value
+        // so we can make sure we get exactly what we asked for
+        
+        Map <String,String> actualResultMap = jp.getMap("",String.class,String.class);
+        // do not need to remove extra links from json result
+        // because we are checking key value pair , anything we dont check will not matter
+       // actualResultMap.remove("links");
+        System.out.println("actualResultMap = " + actualResultMap);
+
+        DB_Utility.runQuery("SELECT * FROM REGIONS WHERE REGION_ID = "+myID ) ;
+        Map<String,String> expectedResultMap = DB_Utility.getRowMap(1);
+        System.out.println("expectedResultMap = " + expectedResultMap);
+// since the keyname is different in both map we can not directly
+        // compare map to map object
+        // we have to compare the value of key step by step
+
+          assertThat(actualResultMap.get("region_id"), equalTo(expectedResultMap.get("REGION_ID")));
+          assertThat(actualResultMap.get("region_name"), equalTo(expectedResultMap.get("REGION_NAME")));
+    }
+
+    @DisplayName("Testing GET /regions/{region_id} Data Match Database Data With Just value by value")
+    @Test
+    public void testRegionDataFromResponseMatchDB_Data3() {
+        int myID = 3;
+        JsonPath jp = given()
+                .pathParam("region_id", myID).
+                        when()
+                .get("/regions/{region_id}").
+                        then()
+                .log().body()
+                .statusCode(200)
+                .extract()
+                .jsonPath();
+
+        String actualRegionId = jp.getString("region_id");
+        String actualRegionName = jp.getString("region_name");
+        DB_Utility.runQuery("SELECT REGION_ID,REGION_NAME FROM REGIONS WHERE REGION_ID = "+myID ) ;
+        String expectedRegionId = DB_Utility.getColumnDataAtRow(1,"REGION_ID");
+        String expectedRegionName = DB_Utility.getColumnDataAtRow(1,"REGION_NAME");
+
+
+        assertThat(actualRegionId,is(expectedRegionId));
+        assertThat(actualRegionName,equalTo(expectedRegionName));
+
 
     }
 
