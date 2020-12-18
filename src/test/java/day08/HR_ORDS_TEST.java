17b164bbd11d4d4ba9d9603bb3d42c8f52be98bc
Alex-B-I <alex.berloh@gmail.com>
2020-12-17 18:41:07 -0600

    day8

--- src/test/java/day08/HR_ORDS_TEST.java
+++ src/test/java/day08/HR_ORDS_TEST.java
@@ -5,6 +5,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 import pojo.BookCategory;
+import pojo.Country;
 import pojo.Region;
 
 import java.util.List;
@@ -32,6 +33,36 @@ public class HR_ORDS_TEST {
         reset();
     }
 
+    @DisplayName("Test GET / countries/ {country_id} to POJO")
+    @Test
+    public void testCountryResponseToPOJO(){
 
+      //  Response response = get("/countries/{country_id}","AR").prettyPeek();
+        Response response =
+                            given()
+                                   .pathParam("country_id","AR").
+                            when()
+                                    .get("/countries/{country_id}").prettyPeek();
+
+        Country ar = response.as(Country.class);
+        System.out.println("Argentina = "+ar);
+
+        Country ar1 = response.jsonPath().getObject("",Country.class);
+        System.out.println("Argentina with jsonPath = "+ar1);
+
+
+    }
+
+    @DisplayName("Test GET /countries to List of POJO")
+    @Test
+    public void testAllCountriesResponseToListOfPOJO(){
+        Response response = get("/countries").prettyPeek();
+
+        List<Country>  countryList =response.jsonPath().getList("items",Country.class);
+
+        countryList.forEach(System.out::println);
+
+
+    }
 
 }
