da188779eb4ab0387765416b3623ba2edcabe8df
Alex-B-I <alex.berloh@gmail.com>
2020-12-20 13:37:51 -0600

    day10 Endpoint negative scenario

--- src/test/java/day10/SpartanWithReusableSpecForAdminRoleTest.java
+++ src/test/java/day10/SpartanWithReusableSpecForAdminRoleTest.java
@@ -4,7 +4,6 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import pojo.Spartan;
 import utility.ConfigurationReader;
@@ -20,40 +19,50 @@ public class SpartanWithReusableSpecForAdminRoleTest {
 
     static RequestSpecification givenSpec ;
     static ResponseSpecification thenSpec ;
+    static RequestSpecification postReqSpec ;
+    static Spartan randomSpartanPayload ;
 
     @BeforeAll
     public static void setUp(){
+
         baseURI = ConfigurationReader.getProperty("spartan.base_url");
         basePath = "/api" ;
 
         givenSpec =  given().log().all()
                 .auth().basic("admin","admin") ;
+        // log().all() will not work with expect()
+        // in order to make it work we need to use different method
+        // logDetail(LogDetail.ALL) to provide how much we want to log
         thenSpec = expect().logDetail(LogDetail.ALL)
                 .statusCode(is(200) )
                 .contentType(ContentType.JSON) ;
+
+        randomSpartanPayload = SpartanUtil.getRandomSpartanPOJO_Payload();
+        postReqSpec = given().spec(givenSpec)
+                .contentType(ContentType.JSON)
+                .body(randomSpartanPayload) ;
     }
 
     @DisplayName("GET /api/spartans/{id} Endpoint Test")
     @Test
     public void testOneSpartan(){
+
         given()
                 .spec(givenSpec)
                 .pathParam("id",34).
-        when()
+                when()
                 .get("/spartans/{id}").
-        then()
-                .spec(thenSpec);
-
-//======================================================
-        //alternative way, since the data type of givenSpec is already a RequestSpecification
-
-
+                then()
+                .spec(thenSpec)
+        ;
+        // alternative way , since the data type of givenSpec is already a RequestSpecification
         givenSpec
                 .pathParam("id",34).
-        when()
+                when()
                 .get("/spartans/{id}").
-        then()
-                .spec(thenSpec);
+                then()
+                .spec(thenSpec)
+        ;
 
     }
 
@@ -61,31 +70,44 @@ public class SpartanWithReusableSpecForAdminRoleTest {
     @Test
     public void testPost1Data() {
 
-        Spartan randomSpartanPayload = SpartanUtil.getRandomSpartanPOJO_Payload();
+
 
         RequestSpecification postReqSpec =  given().spec(givenSpec)
-                                            .contentType(ContentType.JSON)
-                                            .body(randomSpartanPayload) ;
+                .contentType(ContentType.JSON)
+                .body(randomSpartanPayload) ;
 
-        ResponseSpecification postResponseSpec = expect().logDetail(LogDetail.ALL)
-                                                    .statusCode(is(201))
-                                                    .contentType(ContentType.JSON)
-                                                    .body("success",is("A Spartan is Born!"))
-                                                    .body("data.id", notNullValue())
-                                                    .body("data.name",is(randomSpartanPayload.getName()))
-                                                    .body("data.gender",is(randomSpartanPayload.getGender()))
-                                                    .body("data.phone",is(randomSpartanPayload.getPhone()));
-
+        ResponseSpecification postResponseSpec =  expect().logDetail(LogDetail.ALL)
+                .statusCode(is(201))
+                .contentType(ContentType.JSON)
+                .body("success",is("A Spartan is Born!") )
+                .body("data.id",notNullValue() )
+                .body("data.name", is( randomSpartanPayload.getName() ) )
+                .body("data.gender", is(randomSpartanPayload.getGender() ))
+                .body("data.phone", is(randomSpartanPayload.getPhone()) )
+                ;
         given()
                 .spec(postReqSpec).
+                when()
+                .post("/spartans").
+                then()
+                .spec(postResponseSpec)
+        ;
+    }
+
+    @DisplayName("Test POST /api/spartans Endpoint negative scenario ")
+    @Test
+    public void testBadRequest400responseBody(){
+
+        Spartan badPayload = new Spartan("A","A",100L) ;
+        given()
+                .spec(postReqSpec)
+                .body(badPayload).
         when()
                 .post("/spartans").
         then()
-                .spec(postResponseSpec);
-
+                .statusCode(400) ;
 
     }
-
 
 
 
@@ -94,9 +116,8 @@ public class SpartanWithReusableSpecForAdminRoleTest {
     @DisplayName("GET /api/spartans Endpoint Test")
     @Test
     public void testAllSpartan(){
-        // log().all() will not work with expect()
-        // in order to make it work we need to use different method
-        // logDetail(LogDetail.ALL) to provide how much we want to log
+
+
         given()
                 .spec(givenSpec).
                 when()
@@ -104,19 +125,29 @@ public class SpartanWithReusableSpecForAdminRoleTest {
                 then()
                 .spec(thenSpec)
         ;
+
+
     }
+
 
     @DisplayName("GET /api/spartans check response time < 2 second")
     @Test
     public void testResponseTime(){
+
         given()
                 .spec(givenSpec).
-        when()
+                when()
                 .get("/spartans").
-        then()
+                then()
                 .spec(thenSpec)
-                .time(lessThan(2000L) )
-                .time(lessThan(2L), TimeUnit.SECONDS);
+                .time( lessThan(2000L) )
+                .time( lessThan(2L)  , TimeUnit.SECONDS )
+
+        ;
     }
+
+
+
+
 
 }
