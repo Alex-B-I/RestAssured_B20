6fc9c5594371f069f0be1d913194de849e18ccd1
Alex-B-I <alex.berloh@gmail.com>
2020-12-20 09:06:04 -0600

    day9

--- src/test/java/day09/JUnit5_ParametrizedTest.java
+++ src/test/java/day09/JUnit5_ParametrizedTest.java
@@ -1,7 +1,13 @@
 package day09;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class JUnit5_ParametrizedTest {
     
@@ -12,7 +18,45 @@ public class JUnit5_ParametrizedTest {
     
         //assert 5,6,7,8,9 all less than 10
         System.out.println("myNumber = " + myNumber);
-        
+        assertTrue(myNumber <10);
     }
-    
+    //using CSV file as source for parameterized test
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/zipcode.csv",numLinesToSkip = 1)
+    public void test2(String zip){
+        System.out.println("zip = " + zip);
+        //sending request to zipcode endpoint here
+
+        //api.zippopotam.us/us/{zipcode}
+        // baseurl : api.zippopotam.us
+        //  endpoint is /us/{zipcode}
+        given()
+                .log().uri()
+                .baseUri("https://api.zippopotam.us")
+                .pathParam("zipcode" , zip).
+                when()
+                .get("/us/{zipcode}").
+                then()
+                .statusCode(200) ;
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/country_zip.csv" , numLinesToSkip = 1)
+    public void testCountryZipCodeCombo(String csvCountry, int csvZip){
+
+        given()
+                .log().uri()
+                .baseUri("https://api.zippopotam.us")
+                .pathParam("country" , csvCountry)
+                .pathParam("zipcode" , csvZip).
+                when()
+                .get("/{country}/{zipcode}").
+                then()
+                .statusCode(200) ;
+
+    }
+
+
+
 }
